4b025c5e73b43d7360e1faca8bdaa16e69357c86
pzalejski <piotrzalejski@outlook.com>
2020-10-06 22:45:17 -0500

    cleaned up code into 2 functions - pz

--- bitcoin_notifications/sms.py
+++ bitcoin_notifications/sms.py
@@ -7,28 +7,36 @@ em = email
 pas = password
 number = pnum
 
-sms_gateway = pnum+'@txt.att.net'
+sms_gateway = pnum + '@txt.att.net'
+server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 
-try:
-    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server_ssl.ehlo()
-    server_ssl.login(em,pas)
-except:
-    print('sometihng went wrong')
+def email_login(em, pas):
 
-# use the MIME module to structure the message
-msg = MIMEMultipart()
-msg['From'] = em
-msg['To'] = sms_gateway
-# Make sure you add a new line in the subject
-msg['Subject'] = "Loren imspum\n"
-# Make sure you also add new lines to your body
-body = "Loren impsum\n"
-# and then attach that body furthermore you can also send html content.
-msg.attach(MIMEText(body, 'plain'))
+    try:
+        server_ssl.ehlo()
+        server_ssl.login(em,pas)
+    except:
+        print('sometihng went wrong')
 
-sms = msg.as_string()
+def bitcoin_sms(em, sms_gateway, sms_subject, sms_body):
+    email_login(em, pas)
 
-server_ssl.sendmail(em,sms_gateway,sms)
-# lastly quit the server
-server_ssl.quit()
+    # use the MIME module to structure the message
+    msg = MIMEMultipart()
+    msg['From'] = em
+    msg['To'] = sms_gateway
+    # Make sure you add a new line in the subject
+    msg['Subject'] = sms_subject
+    # Make sure you also add new lines to your body
+    body = sms_body
+    # and then attach that body furthermore you can also send html content.
+    msg.attach(MIMEText(body, 'plain'))
+
+    sms = msg.as_string()
+
+    server_ssl.sendmail(em,sms_gateway,sms)
+    # lastly quit the server
+    server_ssl.quit()
+
+
+
