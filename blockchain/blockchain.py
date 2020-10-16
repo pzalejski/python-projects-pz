60a6cf01230eb13d7dab4f9e3773631c12105673
pzalejski <piotrzalejski@outlook.com>
2020-10-15 21:06:08 -0500

    new_block and hash methods created - pz

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,20 +1,71 @@
+import hashlib, json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # creates a new block and adds it to the chain
-        pass
+        # create the starting block
+        self.new_block(previous_hash=1, proof=100)
+        
+
+    def new_block(self, proof, previous_hash=None):
+       '''Create a new block in the blockchain
+
+       Parameters
+       -----------
+       proof: (int)
+            the proof given by the proof of work algorithm
+
+
+       previous_hash:(optional) (str)
+            hash of previous block
+       
+       Returns
+       -----------
+       return: (dict) 
+            New block '''
+       
+       block = {
+           'index': len(self.chain) + 1,
+           'timestamp': time(),
+           'transactions': self.current_transactions,
+           'proof': proof,
+           'previous_hash': previous_hash or self.hash(self.chain[-1])
+       }
+
+       # reset the current list of transactions
+       self.current_transactions = []
+       
+       self.chain.append(block)
+       return block
+
 
     def new_transaction(self, sender, recipient, amount):
         '''
         Creates a new transaction to go into the next mined block
-        :param sender: <str> Address of the Sender
-        :param recipient: <str> Address of the Recipient
-        :param amount: <int> Amount
-        :return <int> The index of the blcok that will hold this transaction
+
+        Parameters
+        -----------
+        sender: (str)
+            Address of the Sender
+
+
+        recipient: (str)
+            Address of the Recipient
+
+
+        amount: (int)
+            Amount
+
+        Returns
+        -----------
+
+        return: (int)
+            The index of the block that will hold this transaction
         '''
+
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -25,8 +76,22 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
-        # hashes a block
-        pass
+        '''creates a SHA-256 hash of a block
+        
+        Parameters
+       -----------
+       block: (dict)
+            block
+
+       Returns
+       -----------
+        return: (str) '''
+        
+        # we must make sure that the dictionary is ordered, or we'll have inconsistent hashes
+        block_string = json.dump(block, sort_keys=True).encode()
+
+        return hashlib.sha256(block_string).hexdigest()
+
 
     @property
     def last_block(self):
